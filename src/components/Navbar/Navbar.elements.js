ec932cd82df77895fd39279f43d0731d29d9d49d
joyAct <joyleeact@gmail.com>
2020-12-16 20:47:48 +0900

    Editing the Navbar

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Nav = styled.div`
+  background: #aaa;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`;
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+`;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav, NavbarContainer } from './Navbar.elements';
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>TEXT</NavbarContainer>
+      </Nav>
+    </>
+  );
+}
+
+export default Navbar;
