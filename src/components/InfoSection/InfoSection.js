6f53373a8033c68d325186d5f2d6705b0ab76ff1
joyAct <joyleeact@gmail.com>
2020-12-18 23:31:09 +0900

    Creating a Info Section of Home Page

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { InfoSec } from './InfoSection.elements';
+
+function InfoSection({ lightBg }) {
+  return (
+    <>
+      <InfoSec lightBg={lightBg}>Hello</InfoSec>
+    </>
+  );
+}
+
+export default InfoSection;
