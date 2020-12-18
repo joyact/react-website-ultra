cdca5a5280eee2ee1a3b8ea659cd8a0521b9d283
joyAct <joyleeact@gmail.com>
2020-12-18 23:57:26 +0900

    InfoSection Style

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { InfoSec } from './InfoSection.elements';
+import { Link } from 'react-router-dom';
+import {
+  InfoSec,
+  InfoRow,
+  InfoColumn,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+} from './InfoSection.elements';
+import { Button, Container } from '../../globalStyle';
 
-function InfoSection({ lightBg }) {
+function InfoSection({
+  lightBg,
+  imgStart,
+  lightTopLine,
+  lightText,
+  lightTextDesc,
+}) {
   return (
     <>
-      <InfoSec lightBg={lightBg}>Hello</InfoSec>
+      <InfoSec lightBg={lightBg}>
+        <Container>
+          <InfoRow imgStart={imgStart}>
+            <InfoColumn>
+              <TextWrapper>
+                <TopLine lightTopLine={lightTopLine}></TopLine>
+                <Heading lightText={lightText}></Heading>
+                <Subtitle lightTextDesc={lightTextDesc}></Subtitle>
+                <Link to="/sign-up">
+                  <Button big fontBig></Button>
+                </Link>
+              </TextWrapper>
+            </InfoColumn>
+          </InfoRow>
+        </Container>
+      </InfoSec>
     </>
   );
 }
