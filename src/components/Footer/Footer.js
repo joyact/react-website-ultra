e99923521079adc23c6277fcdb8fa30f9a9c126f
joyAct <joyleeact@gmail.com>
2020-12-21 15:08:06 +0900

    Adding Social Media Links in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
 import { Button } from '../../globalStyle';
 import {
   FooterContainer,
@@ -12,6 +19,13 @@ import {
   FooterLinkItems,
   FooterLinkTitle,
   FooterLink,
+  SociaMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcon,
+  SocialIcons,
+  SocialIconLink,
 } from './Footer.elements';
 
 function Footer() {
@@ -61,6 +75,37 @@ function Footer() {
           </FooterLinkItems>
         </FooterLinksWrapper>
       </FooterLinksContainer>
+      <SociaMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon />
+            Ultra
+          </SocialLogo>
+          <WebsiteRights>ULTRA &copy; 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href="/"
+              target="_blank"
+              aria-label="Youtube"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SociaMedia>
     </FooterContainer>
   );
 }
