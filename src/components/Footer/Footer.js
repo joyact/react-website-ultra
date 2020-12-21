0ca3578e38eba20a6b2f83276d7d37c1b318fe51
joyAct <joyleeact@gmail.com>
2020-12-21 14:22:32 +0900

    Adding Page Links in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,11 @@ import {
   FooterSubText,
   Form,
   FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
 } from './Footer.elements';
 
 function Footer() {
@@ -22,6 +27,40 @@ function Footer() {
           <Button fontBig>Subscribe</Button>
         </Form>
       </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Carrers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contack Us</FooterLinkTitle>
+            <FooterLink to="/">Contact</FooterLink>
+            <FooterLink to="/">Support</FooterLink>
+            <FooterLink to="/">Destinations</FooterLink>
+            <FooterLink to="/">Sponsorships</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to="/">Submit Video</FooterLink>
+            <FooterLink to="/">Ambassadors</FooterLink>
+            <FooterLink to="/">Agency</FooterLink>
+            <FooterLink to="/">Influencer</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to="/">Instagram</FooterLink>
+            <FooterLink to="/">Facebook</FooterLink>
+            <FooterLink to="/">Youtube</FooterLink>
+            <FooterLink to="/">Twitter</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
     </FooterContainer>
   );
 }
