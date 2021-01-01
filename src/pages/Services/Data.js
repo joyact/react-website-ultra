4afe6ec6323fef1886c41d2cc89bfd4b11f2df7f
joyAct <joyleeact@gmail.com>
2021-01-01 17:26:11 +0900

    Adding the Rest of Pages

---
+++ src/pages/Services/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Marketing Agency',
+  headline: 'Lead Generation Specialist for Online Businesses',
+  description:
+    'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  buttonLabel: 'Get Started',
+  img: require('../../images/svg1.svg').default,
+  alt: 'Image',
+  start: '',
+};
+
+export const homeObjTwo = {
+  primary: false,
+  lightBg: true,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Instant Setup',
+  headline: 'Extremely quick onboarding process',
+  description:
+    "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+  buttonLabel: 'Learn More',
+  img: require('../../images/svg2.svg').default,
+  alt: 'Image',
+  start: '',
+};
+
+export const homeObjThree = {
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Sarah Jeni',
+  headline:
+    'Ultra helped me increase my revenue by over 3X in less than 3 months!',
+  description:
+    "Their team is wonderful! I can't believe I didn't start working with them earlier.",
+  buttonLabel: 'View Case Study',
+  img: require('../../images/svg3.svg').default,
+  alt: 'Image',
+  start: 'true',
+};
+
+export const homeObjFour = {
+  primary: false,
+  lightBg: true,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Secure Database',
+  headline: 'All your data is stored on our secure server',
+  description:
+    'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+  buttonLabel: 'Sign Up Now',
+  img: require('../../images/svg4.svg').default,
+  alt: 'Image',
+  start: 'true',
+};
